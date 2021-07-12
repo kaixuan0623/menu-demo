a89a1126726caa12d1167deb628b649f9b68892f
KaixuanHu <kaixuan.hu@streamworx.ai>
2021-07-12 19:34:35 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # menu-demo
+
+Project for react self teaching, adapted from: https://github.com/MitchGabriel/react-restaurant-menu
---
+++ src/data.js
@@ -0,0 +1,95 @@
+const menu = [
+    // {
+    //     "category": "Hors d'oeuvres",
+    //     "id": 0,
+    //     "title": "Orange",
+    //     "img": "https://producemadesimple.ca/wp-content/uploads/2015/01/orange-web-1080x810.jpg",
+    //     "desc": "Very nice fruit",
+    //     "price": 0
+    // },
+    // {
+    //     "category": "Hors d'oeuvres",
+    //     "id": 1,
+    //     "title": "Yogurt",
+    //     "img": "https://www.daringgourmet.com/wp-content/uploads/2019/10/How-to-Make-Yogurt-4.jpg",
+    //     "desc": "Good for your health!",
+    //     "price": 0
+    // },
+    {
+        "category": "Soup",
+        "id": 6,
+        "title": "Corn carrot pork bone soup 玉米胡萝卜排骨老火靓汤",
+        "img": "https://www.ohmyfoodrecipes.com/wp-content/uploads/2018/09/corn-carrot-soup.jpg",
+        "desc": "Tasty sweet corn carrot soup is a classic and common Cantonese soup. ",
+        "price": 0
+    },
+    {
+        "category": "Soup",
+        "id": 7,
+        "title": "Tomato Egg soup 西红柿鸡蛋汤",
+        "img": "https://thewoksoflife.com/wp-content/uploads/2019/06/tomato-egg-drop-soup-10.jpg",
+        "desc": "很有营养的汤！classic Chinese soup, Bowls of tomato egg drop soup are commonly seen on tables in Chinese households, especially in the summertime when tomatoes are plentiful.",
+        "price": 0
+    },
+    {
+        "category": "Fish and Meat",
+        "id": 11,
+        "title": "CANTONESE STEAMED FISH 清蒸鲈鱼",
+        "img": "https://thewoksoflife.com/wp-content/uploads/2020/12/cantonese-steamed-fish-17-1089x1536.jpg",
+        "desc": "Cantonese steamed fish is a traditional, simple dish often served as one of the final courses in a Chinese wedding banquet. 非常好吃！",
+        "price": 0
+    },
+    {
+        "category": "Fish and Meat",
+        "id": 12,
+        "title": "CHINESE BRAISED FISH (HONG SHAO YU) 红烧鲈鱼",
+        "img": "https://thewoksoflife.com/wp-content/uploads/2015/02/chinese-braised-fish-10.jpg",
+        "desc": "红烧鲈鱼也是不错的。",
+        "price": 0
+    },
+
+    {
+        "category": "Fish and Meat",
+        "id": 13,
+        "title": "猪肉白菜炖粉条",
+        "img": "https://i.ytimg.com/vi/pmW3SWpP2ig/maxresdefault.jpg",
+        "desc": "猪肉白菜炖粉条，好吃的！是四川菜也是东北菜，发源于四川，在唐朝期间薛仁贵非常喜欢四川菜，薛仁贵被政府发配到东北后，他也把四川菜带来东北，并根据当地的风格将其发展成了东北菜系。我的版本是清淡版。",
+        "price": 0
+    },
+
+    {
+        "category": "Vegetable",
+        "id": 15,
+        "title": "炒生菜",
+        "img": "https://assets.epicurious.com/photos/57c5b5d8cf9e9ad43de2d969/6:4/w_620%2Ch_413/stir-fried-garlic-lettuce.jpg",
+        "desc": "蒜蓉炒生菜，好吃又营养！",
+        "price": 0
+    },
+
+    {
+        "category": "Main Course",
+        "id": 20,
+        "title": "Rice",
+        "img": "https://assets.epicurious.com/photos/57d70c8ade27564257b657c6/2:1/w_1260%2Ch_630/perfect-steamed-rice.jpg",
+        "desc": "You know, of course.",
+        "price": 0
+    },
+    {
+        "category": "Main Course",
+        "id": 21,
+        "title": "Spaghetti",
+        "img": "https://i.ytimg.com/vi/jBoMTZIHqLc/maxresdefault.jpg",
+        "desc": "煮熟后加加酱油或者pasta source",
+        "price": 0
+    },
+    {
+        "category": "Dessert",
+        "id": 25,
+        "title": "Choco pie",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/7/74/Chocolate_Pie.jpg",
+        "desc": "A choco pie is a snack cake consisting of two small round layers of cake with marshmallow filling and a chocolate covering. The term originated in America but is now also used widely in South Korea, Japan, and its exports, and many other countries as either a brand name or a generic term.",
+        "price": 0
+    }
+];
+
+export default menu;
